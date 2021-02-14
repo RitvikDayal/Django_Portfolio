ec18a2eff85546c25c835919dea40fbf5930ac63
Ritvik Dayal <ritvikr1605@gmail.com>
2021-02-14 13:27:57 -0800

    added lan IP

--- core/settings.py
+++ core/settings.py
@@ -10,7 +10,7 @@ SECRET_KEY = os.environ.get("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1']
+ALLOWED_HOSTS = ['127.0.0.1', '192.168.65.129']
 
 # Application definition
 
