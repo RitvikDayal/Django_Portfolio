095e6727e5b4530da9993f3a49f0abdd41bddfe4
Ritvik Dayal <ritvikr1605@gmail.com>
2021-03-01 02:45:12 -0800

    updated database settings

--- core/settings.py
+++ core/settings.py
@@ -1,6 +1,7 @@
 import os
 from pathlib import Path
 import django_heroku
+import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -137,3 +138,6 @@ RECAPTCHA_PRIVATE_KEY = os.environ.get('RECAPTCHA_PRIVATE_KEY')
 
 RECAPTCHA_DOMAIN = 'www.recaptcha.net'
 
+db_from_env = dj_database_url.config() 
+
+DATABASES['default'].update(db_from_env) 
