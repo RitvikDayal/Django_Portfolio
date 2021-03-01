d8c788f1d6ea1956cbb9e24779150068dfb907a7
Ritvik Dayal <ritvikr1605@gmail.com>
2021-03-01 03:13:18 -0800

    updated host

--- core/settings.py
+++ core/settings.py
@@ -12,7 +12,7 @@ SECRET_KEY = os.environ.get("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True if os.environ.get("DEBUG") == 'True' else False
 
-ALLOWED_HOSTS = ['127.0.0.1', 'ritvikdayal.herokuapp.com']
+ALLOWED_HOSTS = ['www.ritvikdayal.herokuapp.com']
 
 # Application definition
 
