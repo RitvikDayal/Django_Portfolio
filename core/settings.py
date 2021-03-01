81ac1cc8ce497fb2eb1e43768589e14746f71901
Ritvik Dayal <ritvikr1605@gmail.com>
2021-03-01 03:38:37 -0800

    updated static files

--- core/settings.py
+++ core/settings.py
@@ -10,7 +10,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True if os.environ.get("DEBUG") == 'True' else False
+DEBUG = False
 
 ALLOWED_HOSTS = ['www.ritvikdayal.herokuapp.com']
 
@@ -28,6 +28,39 @@ INSTALLED_APPS = [
     'crispy_forms',
     'captcha',
 ]
+
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'formatters': {
+        'verbose': {
+            'format' : "[%(asctime)s] %(levelname)s [%(name)s:%(lineno)s] %(message)s",
+            'datefmt' : "%d/%b/%Y %H:%M:%S"
+        },
+        'simple': {
+            'format': '%(levelname)s %(message)s'
+        },
+    },
+    'handlers': {
+        'file': {
+            'level': 'DEBUG',
+            'class': 'logging.FileHandler',
+            'filename': 'mysite.log',
+            'formatter': 'verbose'
+        },
+    },
+    'loggers': {
+        'django': {
+            'handlers':['file'],
+            'propagate': True,
+            'level':'DEBUG',
+        },
+        'portfolio': {
+            'handlers': ['file'],
+            'level': 'DEBUG',
+        },
+    }
+}
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
